2668a9cb8c961d9c7c68009a307f581acae0c196
Santos Argueta <51866484+dkmclarito@users.noreply.github.com>
2021-10-08 17:11:24 -0600

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    var altura = $('.fixed-top2').offset().top;
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > altura) {
+            $('.fixed-top2').addClass('top-det');
+        } else {
+            $('.fixed-top2').removeClass('top-det');
+        }
+    });
+
+});
