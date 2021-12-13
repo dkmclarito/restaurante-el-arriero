5d4723c2d5595c8bc8dddd448d87c65ffcac288c
Santos Argueta <51866484+dkmclarito@users.noreply.github.com>
2021-12-13 12:53:59 -0600

    Conexion a base de datos CONTACTO

---
+++ conexion.php
@@ -0,0 +1,36 @@
+<?php
+
+    $cons_usuario="root";
+    $cons_contra="";
+    $cons_base_datos="prueba";
+    $cons_equipo="localhost";
+    
+    $obj_conexion = mysqli_connect($cons_equipo,$cons_usuario,$cons_contra,$cons_base_datos);
+
+    if(!$obj_conexion)
+    {
+        echo "<h3>No se ha podido conectar PHP - MySQL, verifique sus datos.</h3><hr><br>";
+    }
+    else
+    {
+        echo "<h3>Conexion Exitosa PHP - MySQL</h3><hr><br>";
+    }
+
+
+    $nombre = $_POST['nombre'];
+    $telefono = $_POST['telefono'];
+    $mensaje = $_POST['mensaje'];
+
+    $sql = "INSERT INTO datos VALUES('$nombre', '$telefono', '$mensaje')";
+
+    $var_resultado = $obj_conexion->query($sql);
+
+    if(!$var_resultado){
+        echo"Hubo algun error";
+
+    }else{
+        echo "<script>location.href ='mensajeenviado.php';</script>";
+    }
+
+
+?>
